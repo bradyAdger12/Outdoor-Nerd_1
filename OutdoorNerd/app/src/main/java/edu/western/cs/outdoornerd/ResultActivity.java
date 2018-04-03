1fdc15504bba6f863373fa781128fd58816e6072
bradyAdger12 <35546510+bradyAdger12@users.noreply.github.com>
2018-04-03 09:38:29 -0600

    Add files via upload

--- OutdoorNerd/app/src/main/java/edu/western/cs/outdoornerd/ResultActivity.java
+++ OutdoorNerd/app/src/main/java/edu/western/cs/outdoornerd/ResultActivity.java
@@ -9,7 +9,8 @@ import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import java.util.ArrayList;
+import static edu.western.cs.outdoornerd.Table.hMap;
+import static edu.western.cs.outdoornerd.Table.tNameList;
 
 public class ResultActivity extends AppCompatActivity {
 
@@ -24,38 +25,56 @@ public class ResultActivity extends AppCompatActivity {
         LinearLayout qTable = findViewById(R.id.queryTables);
         LinearLayout dateLayout = findViewById(R.id.Date_Layout);
 
-        DummyAL dummyAL = new DummyAL();
-
-        ArrayList<String> time =  dummyAL.getDummytime1();
-        ArrayList<Integer> temperature = dummyAL.getDummytemp1();
-        ArrayList<Integer> wind = dummyAL.getDummywind1();
-        ArrayList<Integer> rain = dummyAL.getDummyrain1();
-
-        Log.d("dummydata", String.valueOf(time.size()));
-
-
-        DummyData dummyData = new DummyData(time, temperature, wind, rain);
-
 
 
         //This is how to enter data. Date, Table Name, Date Layout, Title Layout, Results layout, Context.
-        Table t1 = new Table("Temp",dateLayout, titleBar, qTable,this);
-
+        Table t1 = new Table("1/15/1995", dateLayout, titleBar, qTable,this);
+        Table t2 = new Table("6/5/1997", dateLayout, titleBar, qTable, this);
+        Table t3 = new Table("4/7/1962", dateLayout, titleBar, qTable, this);
+        Table t4 = new Table("6/23/1984", dateLayout, titleBar, qTable, this);
+        Table t5 = new Table("1/7/1954", dateLayout, titleBar, qTable, this);
+        Table t6 = new Table("6/23/1984", dateLayout, titleBar, qTable, this);
+        Table t7 = new Table("3/8/1971", dateLayout, titleBar, qTable, this);
+        Table t8 = new Table("12/7/1964", dateLayout, titleBar, qTable, this);
+        Table t9 = new Table("2/12/1984", dateLayout, titleBar, qTable, this);
+        Table t10 = new Table("4/20/1969", dateLayout, titleBar, qTable, this);
         //this is how to add data to a particular table. Needs Work to add multiple queries.
+        t1.addResult("Temp", "19F");
+        t1.addResult("Wind", "N/A");
+        t2.addResult("Temp", "24F");
+        t2.addResult("Wind", "N/A");
+        t3.addResult("Temp", "23F");
+        t3.addResult("Wind", "N/A");
+        t4.addResult("Temp", "23F");
+        t4.addResult("Wind", "16mph");
+        t5.addResult("Temp", "23F");
+        t5.addResult("Wind", "18mph");
+        t6.addResult("Temp", "23F");
+        t6.addResult("Wind", "N/A");
+        t7.addResult("Temp", "N/A");
+        t7.addResult("Wind", "18mph");
+        t8.addResult("Temp", "N/A");
+        t8.addResult("Wind", "N/A");
+        t9.addResult("Temp", "18F");
+        t9.addResult("Wind", "18mph");
+        t10.addResult("Temp", "32F");
+        t10.addResult("Wind", "18mph");
 
-        for (int i = 0; i < time.size(); i++){
-            t1.addResult(time.get(i), String.valueOf(temperature.get(i)));
-        }
-
-
-
-        //t1.addResult("8/2/1799", "85F");
 
 
 
 
 
 
+
+
+
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        hMap.clear();
+        tNameList.clear();
 
 
 
